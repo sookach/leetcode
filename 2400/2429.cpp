c676115d56c67afb42555ff56de880cfd66e592a
Andrew Sukach <andrewsukach@gmail.com>
2025-01-14 19:17:42 -0800

    2429. Minimize XOR

---
+++ 2400/2429.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+  int minimizeXor(int num1, int num2) {
+    for (; popcount<unsigned>(num1) < popcount<unsigned>(num2);)
+      num1 |= ~num1 & -~num1;
+
+    for (; popcount<unsigned>(num1) > popcount<unsigned>(num2);)
+      num1 ^= num1 & -num1;
+
+    return num1;
+  }
+};
