754014705da93590580b4962c8ff3a472b4ef71d
Andrew Sukach <andrewsukach@gmail.com>
2025-01-16 19:45:01 -0800

    2425. Bitwise XOR of All Pairings

--- 2400/2425.cpp
+++ 2400/2425.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
   int xorAllNums(vector<int> &nums1, vector<int> &nums2) {
-    return ranges::fold_left(nums1, 0,
-                             [&](auto &&x, auto &&y) {
-                               return size(nums2) % 2 == 0 ? x : x ^ y;
-                             }) ^
-           ranges::fold_left(nums2, 0, [&](auto &&x, auto &&y) {
-             return size(nums1) % 2 == 0 ? x : x ^ y;
-           });
+    return (size(nums2) % 2 == 0 ? 0 : ranges::fold_left(nums1, 0, bit_xor())) ^
+           (size(nums1) % 2 == 0 ? 0 : ranges::fold_left(nums2, 0, bit_xor()));
   }
 };
